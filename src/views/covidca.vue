e331f96db76f93e3c140576e3088dc4900b2eccf
Benjamin Fernandez <benfernandez.dev@gmail.com>
2020-05-27 16:45:33 -0600

    diseñoy asignacion de data en el diseño V.1

--- src/views/covidca.vue
+++ src/views/covidca.vue
@@ -1,8 +1,119 @@
 <template>
-    <div>
-        
+    <div class="container">
+        <center>
+            <h1>{{seleccion}}</h1>
+            <div class="input-group mb-3">
+                <select v-model="seleccion" class="custom-select" id="inputGroupSelect01">
+                    <option disabled value="">Seleccione una opcion</option>
+                    <option>CENTRO AMERICA</option>
+                    <option>El Salvador</option>
+                    <option>Costa Rica</option>
+                    <option>Nicaragua</option>
+                    <option>Honduras</option>
+                    <option>Guatemala</option>
+                    <option>Panama</option>
+                    <option>Belice</option>
+                </select>
+            </div>
 
-    </div>
+        <div class="row">
+            <div class="card border-danger text-danger bg-dark m-3 col md-6" >
+                <div class="card-header">Muertes</div>
+                <div class="card-body row">
+                    <div class="col sm-4">
+                        <h5 class="card-title">Total</h5>
+                        <p class="card-text" v-if="seleccion == 'El Salvador'">{{es.deaths}}</p>
+                        <p class="card-text" v-if="seleccion == 'Costa Rica'">{{cr.deaths}}</p>
+                        <p class="card-text" v-if="seleccion == 'Nicaragua'">{{ni.deaths}}</p>
+                        <p class="card-text" v-if="seleccion == 'Honduras'">{{hn.deaths}}</p>
+                        <p class="card-text" v-if="seleccion == 'Guatemala'">{{gt.deaths}}</p>
+                        <p class="card-text" v-if="seleccion == 'Panama'">{{pn.deaths}}</p>
+                        <p class="card-text" v-if="seleccion == 'Belice'">{{bl.deaths}}</p>
+                    </div>
+                    <div class="col sm-4">
+                        <h5 class="card-title">Hoy</h5>
+                        <p class="card-text" v-if="seleccion == 'El Salvador'">{{es.todayDeaths}} <span class="badge badge-secondary">Nuevo</span></p>
+                        <p class="card-text" v-if="seleccion == 'Costa Rica'">{{cr.todayDeaths}} <span class="badge badge-secondary">Nuevo</span></p>
+                        <p class="card-text" v-if="seleccion == 'Nicaragua'">{{ni.todayDeaths}} <span class="badge badge-secondary">Nuevo</span></p>
+                        <p class="card-text" v-if="seleccion == 'Honduras'">{{hn.todayDeaths}} <span class="badge badge-secondary">Nuevo</span></p>
+                        <p class="card-text" v-if="seleccion == 'Guatemala'">{{gt.todayDeaths}} <span class="badge badge-secondary">Nuevo</span></p>
+                        <p class="card-text" v-if="seleccion == 'Panama'">{{pn.todayDeaths}} <span class="badge badge-secondary">Nuevo</span></p>
+                        <p class="card-text" v-if="seleccion == 'Belice'">{{bl.todayDeaths}} <span class="badge badge-secondary">Nuevo</span></p>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div class="card border-info text-info bg-dark m-3 col md-6" >
+                <div class="card-header">Casos</div>
+                 <div class="card-body row">
+                    <div class="col sm-6">
+                        <h5 class="card-title">Total</h5>
+                        <p class="card-text" v-if="seleccion == 'El Salvador'">{{es.cases}}</p>
+                        <p class="card-text" v-if="seleccion == 'Costa Rica'">{{cr.cases}}</p>
+                        <p class="card-text" v-if="seleccion == 'Nicaragua'">{{ni.cases}}</p>
+                        <p class="card-text" v-if="seleccion == 'Honduras'">{{hn.cases}}</p>
+                        <p class="card-text" v-if="seleccion == 'Guatemala'">{{gt.cases}}</p>
+                        <p class="card-text" v-if="seleccion == 'Panama'">{{pn.cases}}</p>
+                        <p class="card-text" v-if="seleccion == 'Belice'">{{bl.cases}}</p>
+                    </div>
+                    <div class="col sm-6">
+                        <h5 class="card-title">Activos</h5>
+                        <p class="card-text" v-if="seleccion == 'El Salvador'">{{es.active}}</p>
+                        <p class="card-text" v-if="seleccion == 'Costa Rica'">{{cr.active}}</p>
+                        <p class="card-text" v-if="seleccion == 'Nicaragua'">{{ni.active}}</p>
+                        <p class="card-text" v-if="seleccion == 'Honduras'">{{hn.active}}</p>
+                        <p class="card-text" v-if="seleccion == 'Guatemala'">{{gt.active}}</p>
+                        <p class="card-text" v-if="seleccion == 'Panama'">{{pn.active}}</p>
+                        <p class="card-text" v-if="seleccion == 'Belice'">{{bl.active}}</p>
+                        
+                    </div>
+
+                    <div class="col sm-4">
+                        <h5 class="card-title">Hoy</h5>
+                        <p class="card-text" v-if="seleccion == 'El Salvador'">{{es.todayCases}} <span class="badge badge-secondary">Nuevo</span></p>
+                        <p class="card-text" v-if="seleccion == 'Costa Rica'">{{cr.todayCases}} <span class="badge badge-secondary">Nuevo</span></p>
+                        <p class="card-text" v-if="seleccion == 'Nicaragua'">{{ni.todayCases}} <span class="badge badge-secondary">Nuevo</span></p>
+                        <p class="card-text" v-if="seleccion == 'Honduras'">{{hn.todayCases}} <span class="badge badge-secondary">Nuevo</span></p>
+                        <p class="card-text" v-if="seleccion == 'Guatemala'">{{gt.todayCases}} <span class="badge badge-secondary">Nuevo</span></p>
+                        <p class="card-text" v-if="seleccion == 'Panama'">{{pn.todayCases}} <span class="badge badge-secondary">Nuevo</span></p>
+                        <p class="card-text" v-if="seleccion == 'Belice'">{{bl.todayCases}} <span class="badge badge-secondary">Nuevo</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="card border-warning text-warning bg-dark m-3 col md-6" >
+                <div class="card-header">Criticos</div>
+                <div class="card-body">
+                <h5 class="card-title">Todos</h5>
+                <p class="card-text" v-if="seleccion == 'El Salvador'">{{es.critical}}</p>
+                <p class="card-text" v-if="seleccion == 'Costa Rica'">{{cr.critical}}</p>
+                <p class="card-text" v-if="seleccion == 'Nicaragua'">{{ni.critical}}</p>
+                <p class="card-text" v-if="seleccion == 'Honduras'">{{hn.critical}}</p>
+                <p class="card-text" v-if="seleccion == 'Guatemala'">{{gt.critical}}</p>
+                <p class="card-text" v-if="seleccion == 'Panama'">{{pn.critical}}</p>
+                <p class="card-text" v-if="seleccion == 'Belice'">{{bl.critical}}</p>
+                </div>
+            </div>
+
+            <div class="card border-success text-success bg-dark m-3 col md-6" >
+                <div class="card-header">Recuperados</div>
+                <div class="card-body">
+                <h5 class="card-title">Todos</h5>
+                <p class="card-text" v-if="seleccion == 'El Salvador'">{{es.recovered}}</p>
+                <p class="card-text" v-if="seleccion == 'Costa Rica'">{{cr.recovered}}</p>
+                <p class="card-text" v-if="seleccion == 'Nicaragua'">{{ni.recovered}}</p>
+                <p class="card-text" v-if="seleccion == 'Honduras'">{{hn.recovered}}</p>
+                <p class="card-text" v-if="seleccion == 'Guatemala'">{{gt.recovered}}</p>
+                <p class="card-text" v-if="seleccion == 'Panama'">{{pn.recovered}}</p>
+                <p class="card-text" v-if="seleccion == 'Belice'">{{bl.recovered}}</p>
+                </div>
+            </div>
+
+        </div>
+        </center>
+    </div>  
 </template>
 
 <script>
@@ -10,6 +121,7 @@ import axios from 'axios'
 export default {
     data() {
         return {
+            seleccion : '',
             es : null,
             cr : null,
             ni : null,
@@ -27,7 +139,7 @@ export default {
         this.honduras(),
         this.guatemala(),
         this.panama(),
-        this.belize()
+        this.belice()
     },
 
     methods: {
@@ -111,12 +223,12 @@ export default {
         },
 
 
-        belize(){
+        belice(){
             //PETICION GET DE EL BELIZE
             axios.get('https://coronavirus-19-api.herokuapp.com/countries/belize')
             .then(res => {
                 console.log(res)
-                this.belize = res.data
+                this.belice = res.data
                 this.bl = res.data
             })
             .catch(err => {
