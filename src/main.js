b76f06c982b09cbdbe7d5b603d8eb99890d28197
Benjamin Fernandez <benfernandez.dev@gmail.com>
2020-05-27 13:45:13 -0600

    agregando axios y bootstrap

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+ 
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
